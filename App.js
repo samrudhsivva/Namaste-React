843799435cd8927aaf1397a1e0070642fc959d7b
ssivva <140145406+ssivva@users.noreply.github.com>
2023-10-03 13:31:42 +0530

    assignment-1 code

---
+++ App.js
@@ -0,0 +1,19 @@
+const heading = React.createElement("h1", { id: "heading" }, "hey from React");
+console.log(heading);
+const domRoot = document.getElementById("root-react");
+const root = ReactDOM.createRoot(domRoot);
+
+const parentElement = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1", key: "0" }, [
+    React.createElement("h1", { id: "h1", key: "0" }, "I am H1 TAG"),
+    React.createElement("h2", { id: "h2", key: "1" }, "I am H2 TAG"),
+  ]),
+  React.createElement("div", { id: "child2", key: "1" }, [
+    React.createElement("h1", { id: "h1", key: "0" }, "I am H1 TAG"),
+    React.createElement("h2", { id: "h2", key: "1" }, "I am H2 TAG"),
+  ]),
+]);
+
+root.render(parentElement);
+
+console.log(parentElement);
